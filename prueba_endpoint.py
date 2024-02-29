c99452cd89d579f265908c1103aa977acf0a7982
MRCORD <48929841+MRCORD@users.noreply.github.com>
2024-02-28 19:50:21 -0500

    Added --force

---
+++ prueba_endpoint.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://deployment-intro-ml-service-vqq6pymb5a-uc.a.run.app/v1/prediction'
+
+json = {
+    'opening_gross': 8330681,
+    'screens': 2271,
+    'production_budget': 13000000,
+    'title_year': 1999,
+    'aspect_ratio': 1.85,
+    'duration': 97,
+    'cast_total_facebook_likes': 37907,
+    'budget': 16000000,
+    'imdb_score': 7.2
+}
+
+breakpoint()
